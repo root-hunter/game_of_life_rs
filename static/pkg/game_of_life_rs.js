310f9c51e903d34746182690fca2e2d06a069e83
roothunter <antonio.ricciardi99@outlook.it>
2023-10-09 17:35:16 +0200

    Update statics

--- static/pkg/game_of_life_rs.js
+++ static/pkg/game_of_life_rs.js
@@ -504,11 +504,11 @@ function __wbg_get_imports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper90 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper89 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 31, __wbg_adapter_16);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper247 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper246 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 85, __wbg_adapter_19);
         return addHeapObject(ret);
     };
